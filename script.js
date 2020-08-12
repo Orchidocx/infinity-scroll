dde0c2180b404137cd9aae02dc5a0d280728e8b6
Jacky Li <ilikemacaronii@gmail.com>
2020-08-11 20:29:08 -0700

    improve load for high seo score

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let initialLoad = true;
+let ready = false;
+let imagesLoaded = 30;
+let totalImages = 0;
 let photosArray = [];
-
 // unsplash api
-const count = 10;
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${config.API_KEY || secrets.API_KEY}&count=${count}`;
+let count = 5;
+const API_KEY = config.API_KEY || '4BW62CughI1XO8M6TUf9vSiGr5J-DwQ4KRFx3VtP-sI';
+const url = `https://api.unsplash.com/photos/random/?client_id=${API_KEY}&count=`;
+let apiUrl = `${url}${count}`;
+
+function imageLoaded() {
+  ++imagesLoaded;
+  if(imagesLoaded === totalImages) {
+    ready = true;
+    console.log(`ready = ${ready}`);
+    loader.hidden = true;
+  }
+}
 
 // Helper functions
 function setAttributes(element, attributes) {
@@ -14,7 +28,13 @@ function setAttributes(element, attributes) {
   }
 }
 
+function updateImageLoadCount(amount) {
+  apiUrl = `${url}${amount}`;
+}
+
 function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
   photosArray.forEach((photo) => {
     const item = document.createElement('a');
     setAttributes(item, {
@@ -28,7 +48,8 @@ function displayPhotos() {
       'alt': photo.alt_description,
       'title': photo.alt_description
     });
-
+    // Event Listener, check when each is finished loading
+    img.addEventListener('load', imageLoaded);
     item.appendChild(img);
     imageContainer.appendChild(item);
   });
@@ -40,9 +61,20 @@ async function getPhotos() {
     const response = await fetch(apiUrl, {'Accept-Version':'v1'});
     photosArray = await response.json();
     displayPhotos();
+    if(initialLoad) {
+      updateImageLoadCount(30);
+      initialLoad = false;
+    }
   } catch(err) {
     console.log(err);
   }
 }
+
+window.addEventListener('scroll', () => {
+  if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+    ready = false;
+    getPhotos();
+  }
+});
 
 getPhotos();
